2e87feb886eb908b340da50ba047685fce1cf39e
SensitiveWebUser <55949728+SensitiveWebUser@users.noreply.github.com>
2023-04-04 14:09:00 +0100

    Added All needed Code for Pi Pico

--- RaspberryPiPicoWCode/RaspberryPiPicoWCode.ino
+++ RaspberryPiPicoWCode/RaspberryPiPicoWCode.ino
@@ -1,9 +1,46 @@
+// Sets If Debug State
+static const bool DEBUG_STATE = true;
+
+// Sets Serial Communication type
+#define SERIAL_COM Serial1
+
+// Sets Debuger Serial Communication type
+#define SERIAL_DEBUGER_COM Serial
+
+// Include additional code files
+#include "Util.h"
+#include "WebServer.h"
+
 void setup() {
-  // put your setup code here, to run once:
+
+  // Initialize debuger systems
+  if(DEBUG_STATE){
+    SERIAL_DEBUGER_COM.begin(9600);
+  }
+
+  // Initialize Serial communication
+  SERIAL_COM.begin(9600);
+
+  // Initialize Webserver on Port
+  ServerStartup();
+
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  serverLoop();
+
+  // To allow communication back to client, from Zumo
+  if (SERIAL_COM.available() > 0) {
+
+    // Read the command from the Serial buffer
+    String cmd = SERIAL_COM.readStringUntil('\n');
+
+    //TODO: CODE
+
+    SerialFlush();
+  }
 
 }
