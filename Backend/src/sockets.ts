e1241f0b8668a7967a0f8394c16b3cebb288502f
SensitiveWebUser <55949728+SensitiveWebUser@users.noreply.github.com>
2023-04-04 15:10:45 +0100

    Added listener for movement and call to ZumoAPI

--- Backend/src/sockets.ts
+++ Backend/src/sockets.ts
@@ -3,6 +3,9 @@ import debug, { Debugger } from 'debug';
 import { Server } from 'http';
 import { Server as SocketIO, Socket, ServerOptions } from 'socket.io';
 
+// Import API requests/post from zumoAPI.ts
+import { request, post } from './ZumoApi';
+
 // Create debug logger
 const logger: Debugger = debug('backend:socket');
 
@@ -17,19 +20,24 @@ export default (server: Server) => {
 
   const io = new SocketIO(server, options);
 
-  io.on('connection', (socket: Socket) => {
+  io.on('connection', (socket: Socket): void => {
     logger(`Client connected [id=${socket.id}]`);
 
     //TODO: Check if Raspberry Pi is connected to the server
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', (): void => {
       logger('Client disconnected');
     });
 
-    socket.on('gesture', () => {
+    socket.on('gesture', (): void => {
       logger('Gesture Requested');
       //TODO: Send gesture to frontend via socket.io
       io.emit('gesture', 'idle');
+    });
+
+    socket.on('movement', (movement: string = 'idle'): void => {
+      logger(`Movement Requested ${movement}`);
+      post(`/movement?MOVE=${movement}`);
     });
   });
 };
